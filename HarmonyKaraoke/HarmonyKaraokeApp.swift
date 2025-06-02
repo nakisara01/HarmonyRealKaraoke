21abe980fa7770b2d1a108f15acf2507ece5e5f0
nakisara01 <nakisara01@ajou.ac.kr>
2025-06-03 01:22:57 +0900

    Initial Commit

---
+++ HarmonyKaraoke/HarmonyKaraokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyKaraokeApp.swift
+//  HarmonyKaraoke
+//
+//  Created by 나현흠 on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyKaraokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
